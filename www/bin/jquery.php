bf635f34f877751f54941c8f16a024bb21378425
HotBrain <admin@hotbrain.com.ua>
2018-04-17 19:44:15 +0300

    Update jquery.php

--- www/bin/jquery.php
+++ www/bin/jquery.php
@@ -3,11 +3,11 @@
 	<script src="/libs/bootstrap/js/bootstrap.min.js"></script>
 
 	<!-- Angular Material requires Angular.js Libraries -->
-	<script src="/libs/angularjs/1.6.9/angular.min.js"></script>
-	<script src="/libs/angularjs/1.6.9/angular-animate.min.js"></script>
-	<script src="/libs/angularjs/1.6.9/angular-aria.min.js"></script>
-	<script src="/libs/angularjs/1.6.9/angular-messages.min.js"></script>
+	<!-- <script src="/libs/angularjs/1.6.9/angular.min.js"></script> -->
+	<!-- <script src="/libs/angularjs/1.6.9/angular-animate.min.js"></script> -->
+	<!-- <script src="/libs/angularjs/1.6.9/angular-aria.min.js"></script> -->
+	<!-- <script src="/libs/angularjs/1.6.9/angular-messages.min.js"></script> -->
 
 	<!-- Angular Material Library -->
-	<script src="/libs/angularjs/material/angular-material.min.js"></script>
+	<!-- <script src="/libs/angularjs/material/angular-material.min.js"></script> -->
 	<script src='https://www.google.com/recaptcha/api.js'></script>
