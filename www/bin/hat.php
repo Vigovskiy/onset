ae583de8e99faa8789d99a1455d592f3cf4ddf47
HotBrain <admin@hotbrain.com.ua>
2018-04-17 19:28:50 +0300

    Update hat.php

--- www/bin/hat.php
+++ www/bin/hat.php
@@ -2,9 +2,6 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="/libs/angularjs/material/angular-material.min.css">
-<!--   <link rel="stylesheet" href="/css/STYLES.CSS"> -->
-
-
-<!--  <link rel="stylesheet" href="/libs/font-awesome-4.7.0/css/font-awesome.min.css" type="text/css"> -->
-<!--  <link rel="stylesheet" href="/libs/bootstrap/css/bootstrap.css" type="text/css"> -->
+  <link rel="stylesheet" href="/libs/font-awesome-4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="/libs/bootstrap/css/bootstrap.css" type="text/css">
 </head>
