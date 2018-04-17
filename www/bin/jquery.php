f660c65d77a8e01b5b699bfb59895d95cfcc67a9
HotBrain <admin@hotbrain.com.ua>
2018-04-17 19:43:27 +0300

    Update jquery.php

--- www/bin/jquery.php
+++ www/bin/jquery.php
@@ -1,6 +1,6 @@
-<!--	<script src="/libs/jQuery/jquery-3.2.1.slim.min.js"></script> -->
-<!--	<script src="/libs/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script> -->
-<!--	<script src="/libs/bootstrap/js/bootstrap.min.js"></script> -->
+	<script src="/libs/jQuery/jquery-3.2.1.slim.min.js"></script>
+	<script src="/libs/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
+	<script src="/libs/bootstrap/js/bootstrap.min.js"></script>
 
 	<!-- Angular Material requires Angular.js Libraries -->
 	<script src="/libs/angularjs/1.6.9/angular.min.js"></script>
@@ -10,4 +10,4 @@
 
 	<!-- Angular Material Library -->
 	<script src="/libs/angularjs/material/angular-material.min.js"></script>
-<!--	<script src='https://www.google.com/recaptcha/api.js'></script> -->
+	<script src='https://www.google.com/recaptcha/api.js'></script>
