f8e718148a40e4ab3adb5f9f1dfa3464ea0c0a18
HotBrain <admin@hotbrain.com.ua>
2018-04-17 19:16:19 +0300

    Delete bd.php

--- www/bin/bd.php
+++
@@ -1,5 +0,0 @@
-<?php
-	require "libs/RedBeanPHP/RedBeanPHP4_3_4/rb.php";
-	R::setup('mysql:host=localhost;dbname=kkz.onset.tech','kkz.onset.tech','J00d59SlVOrc4wG1'); //for both mysql or mariaDB
-	R::freeze(false);
-?>
